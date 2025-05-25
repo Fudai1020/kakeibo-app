247870cd31ee620d420002c044b46345dcd9a194
Fudai1020 <fuudai1020@gmail.com>
2025-05-25 09:16:48 +0900

    モーダル入力フォーム作成

--- src/components/TransactionFormModal.tsx
+++ src/components/TransactionFormModal.tsx
@@ -1,46 +1,82 @@
+import { useState } from 'react';
 import '../styles/transactionFormModal.css'
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
 
 type Props = {
   onClose: () => void;
 };
 
 const TransactionFormModal = ({onClose}:Props) => {
+
+  const [type,setType] = useState('income');
+  const [amount,setAmount] = useState("");
+  const [category,setCategory] = useState("");
+  const [memo,setMemo] = useState("");
+  const [date,setDate] = useState("");
+  const [isPrivate,setIsPrivate] = useState(false);
+  const normalizeAmount = (input:string) => {
+    const harf = input.replace(/[０-９]/g,s => String.fromCharCode(s.charCodeAt(0) - 0xFEE0)).replace(/ /g,"");
+    const numeric = harf.replace(/[^0-9]/g,"");
+    return Number(numeric);
+  }
+
+  const handleSubmit = async () => {
+    try{
+      await addDoc(collection(db,"transactions"),{
+        type,
+        amount:normalizeAmount(amount),
+        category,
+        memo,
+        date:new Date(),
+        isPrivate,
+      });
+      alert("登録完了！");
+      onClose();
+    }catch(e){
+      console.error("保存エラー",e);
+      alert("保存に失敗しました");
+    }
+  };
+
   return (
     <div className="modal-container">
       <div className="radio-group">
         <label>
-        <input type="radio" id='income'name="transactionType" value="income"/>
+        <input type="radio" id='income'name="transactionType" value="income" checked={type === 'income'} onChange={() => setType('income')}/>
         収入</label>
         <label>
-        <input type="radio" id='payment' name="transactionType" value="payment"/>
+        <input type="radio" id='payment' name="transactionType" value="payment" checked={type === 'payment'} onChange={() => setType('payment')}/>
         支出</label>
       </div>
       <div className='input-amount'>
-      <input type="text" />
+      <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder='金額'/>
       </div>
       <div className="select-category">
       <h2>カテゴリ</h2>
-      <select name="" id="">
-        <option value=""></option>
-        <option value="">食費</option>
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">選択</option>
+        <option value="食費">食費</option>
       </select>
       </div>
       <div className="memo">
       <h2>メモ</h2>
-      <textarea name="" id=""></textarea>
+      <textarea value={memo} onChange={(e) => setMemo(e.target.value)}></textarea>
       </div>
+      <div className='date-lay'>
       <h2>日付</h2>
-      <input type="date" />
-      <h2>公開設定</h2>
+      <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
+      </div>
       <div className="radio-group">
+          <h2>公開設定</h2>
        <label>
-        <input type="radio" id='isprivate'name="privateselect" value="isprivate"/>
+        <input type="radio" id='isprivate'name="privateselect" value="isprivate" checked={!isPrivate} onChange={() => setIsPrivate(false)}/>
         公開</label>
         <label>
-        <input type="radio" id='isnotprivate' name="privateselect" value="isnotprivate"/>
+        <input type="radio" id='isnotprivate' name="privateselect" value="isnotprivate" checked={isPrivate} onChange={() => setIsPrivate(true)}/>
         非公開</label>
       </div>
-      <button>登録</button>
+      <button className='index-button' onClick={handleSubmit}>登録</button>
     </div>
   )
 }
