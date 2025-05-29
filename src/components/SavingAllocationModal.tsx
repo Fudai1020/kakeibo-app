24f130133c47e5d19b1a2015cad0244a2f8b31ff
Fudai1020 <fuudai1020@gmail.com>
2025-05-30 00:13:23 +0900

    add:firestore保存して表示

--- src/components/SavingAllocationModal.tsx
+++ src/components/SavingAllocationModal.tsx
@@ -1,17 +1,37 @@
 import { useState } from 'react';
 import '../styles/savingAllocationModal.css'
+import { addDoc } from 'firebase/firestore';
+import { collection } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
 type props = {
     onClose:() => void;
     balance:number;
 }
 const SavingAllocationModal = ({onClose,balance}:props) => {
-  const [allocations,setAllocations] = useState<{name:string;amount:number}[]>([]);
+  const [allocations,setAllocations] = useState<{name:string;amount:string}[]>([]);
   const [allocationName,setAllocationName] = useState("");
   const [allocationAmount,setAllocationAmount] = useState("");
+  const [isPrivate,setIsPrivate] = useState("");
+  const handleSave = async() =>{
+    try{
+      const batch = allocations.map((item) => {
+        return addDoc(collection(db,"SavingAllocations"),{
+          name:item.name,
+          amount:Number(item.amount) || 0,
+          isPrivate:isPrivate==='公開',
+          createdAt:new Date()
+        })
+      });
+      await Promise.all(batch);
+      onClose();
+    }catch(error){
+      console.error('保存エラー',error);
+    }
+  }
 
   const handleClick = () => {
     if(allocationName.trim() === "") return;
-    setAllocations([...allocations,{name:allocationName,amount:Number(allocationAmount) || 0}]);
+    setAllocations([...allocations,{name:allocationName,amount:allocationAmount}]);
     setAllocationName("");
     setAllocationAmount("");
   }
@@ -23,14 +43,18 @@ const SavingAllocationModal = ({onClose,balance}:props) => {
         <h2>振り分け先を選ぶ</h2>
         <div className='amount-form'>
         <ul>
-          {allocations.map((item)=>(
-          <li>{item.name}・・・¥<input type="text"/></li>
+          {allocations.map((item,index)=>(
+          <li key={index}>{item.name}・・・¥<input type="input" value={item.amount} onChange={(e) => {
+            const newAllocations = [...allocations];
+            newAllocations[index].amount = e.target.value;
+            setAllocations(newAllocations); 
+          }}/></li>
           ))}
         </ul>
         </div>
         <div className='category-type'>
         <input type="text" placeholder="新しい貯金を入力..." value={allocationName} onChange={(e) => setAllocationName(e.target.value)} />
-        <select>
+        <select value={isPrivate} onChange={(e)=>setIsPrivate(e.target.value)}>
             <option value=""></option>
             <option value="公開">公開</option>
             <option value="非公開">非公開</option>
@@ -38,7 +62,7 @@ const SavingAllocationModal = ({onClose,balance}:props) => {
         <button onClick={handleClick}>追加</button>
         </div>
         <div className='saving-button'>
-        <button onClick={onClose}>保存</button>
+        <button onClick={handleSave}>保存</button>
         </div>
     </div>
   )
