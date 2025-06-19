33aebcd447543df21f3927aa00c179a66cee8b63
Fudai1020 <fuudai1020@gmail.com>
2025-06-20 08:31:10 +0900

    add:編集・削除機能追加

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -2,8 +2,9 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 import AnimateNumber from "./AnimateNumber";
 import SubCategories from "./SubCategories";
 import { db } from "../firebase/firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { useState } from "react";
 
 type Transaction = {
   id: string;
@@ -19,6 +20,9 @@ type Props = {
 };
 
 const CategoryList = ({ transactions }: Props) => {
+    const [editCategory,setEditCategory] = useState<string | null>(null);
+    const [newCategory,setNewCategory] = useState('');
+
   const auth = getAuth();
   const user = auth.currentUser;
 
@@ -48,6 +52,54 @@ const CategoryList = ({ transactions }: Props) => {
     }
   };
 
+  const startEdit = (category:string) =>{
+    setEditCategory(category);
+    setNewCategory(category);
+  }
+  const handleSave = async()=>{
+    if(!editCategory || newCategory.trim() === '') return;
+    if(!user) return;
+
+    const itemsToUpdate = grouped[editCategory];
+    try{
+        const categoriesSnapShot = await getDocs(
+            collection(db,'users',user.uid,'categories')
+        );
+        const matchedDoc = categoriesSnapShot.docs.find((doc)=>doc.data().name === editCategory)
+        const updatePromises = itemsToUpdate.map(item=>{
+            const docRef = doc(db,'users',user.uid,'transactions',item.id);
+            return updateDoc(docRef,{
+                mainCategory:newCategory,
+            });
+        });
+
+        let categoryUpdatePromise = Promise.resolve();
+            if (matchedDoc) {
+      const categoryDocRef = doc(
+        db,
+        'users',
+        user.uid,
+        'categories',
+        matchedDoc.id
+      );
+      categoryUpdatePromise = updateDoc(categoryDocRef, {
+        name: newCategory,
+      });
+    } else {
+      console.warn('該当カテゴリが categories コレクションに見つかりませんでした。');
+    }
+        await Promise.all([...updatePromises,categoryUpdatePromise]);
+        console.log('完了');
+    }catch(error){
+        console.log('失敗',error); 
+    }
+    setEditCategory(null);
+  };
+  const canselEdit = ()=>{
+    setEditCategory(null);
+    setNewCategory('');
+  }
+
   return (
     <div>
       {Object.entries(grouped).map(([mainCategory, subTransactions]) => {
@@ -55,24 +107,48 @@ const CategoryList = ({ transactions }: Props) => {
         return (
           <div key={mainCategory} className="category-block">
             <div className="category-header">
+                {editCategory === mainCategory ?(
+                    <input type="text"
+                    className="edit-category"
+                    value={newCategory} 
+                    onChange={(e)=>setNewCategory(e.target.value)}
+                    onKeyDown={(e)=>{
+                        if(e.key === "Enter") handleSave()
+                        if(e.key === 'escape') canselEdit()}
+                    }
+                    autoFocus
+                    />
+                    
+                ):(
               <h3>
                 <span className="category-title">
                   {mainCategory}・・・合計：<AnimateNumber value={total} />円
                 </span>
               </h3>
+                )}
               <div className="category-actions">
-                <button className="icon-button">
+                {editCategory===mainCategory ? (
+                    <div className="edit-buttons">
+                    <button className='edit-button' onClick={handleSave}>保存</button>
+                    <button className="edit-button" onClick={canselEdit}>キャンセル </button>
+                    </div>
+                ):(
+                <div className="icon-buttons">
+                <button className="icon-button"
+                onClick={()=>startEdit(mainCategory)}>
                   <FaEdit />
                 </button>
+                
                 <button
                   className="icon-button"
                   onClick={() => handleDeleteCategory(mainCategory)}
                 >
                   <FaTrash />
                 </button>
+                </div>
+                )}
               </div>
             </div>
-
             <SubCategories subTransactions={subTransactions} />
           </div>
         );
